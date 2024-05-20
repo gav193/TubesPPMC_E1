686a588e6b324a22855cff05b747867fe3bbfa77
gav193 <116613315+gav193@users.noreply.github.com>
2024-05-20 23:20:52 +0700

    Update backtracking.c

--- backtracking.c
+++ backtracking.c
@@ -1,3 +1,11 @@
+/** EL2208 Praktikum Pemecahan Masalah dengan C 2023/2024
+ *   Modul               : 9 - Tugas Besar
+ *   Hari dan Tanggal    : Senin, 20 Mei 2024
+ *   Nama (NIM)          : Kennard Benaya Manli (13222098)
+ *   Nama File           : backtracking.c
+ *   Deskripsi           : mencari jalur dari input file txt dengan metode backtracking
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
